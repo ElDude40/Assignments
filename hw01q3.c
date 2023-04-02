04900bdc1525508739e05d68adca757dd447e821
ElDude40 <129636672+ElDude40@users.noreply.github.com>
2023-04-01 20:32:46 -0600

    Add files via upload

---
+++ hw01q3.c
@@ -0,0 +1,248 @@
+/**
+*HW01q3
+*Edward Camacho
+*1219284716
+*CSE240
+*16 March 2023
+*/
+
+#include <stdio.h>
+
+void main() {
+
+	char ch = '+';
+
+	float a = 10.0, b = 20.0;
+
+	float f;
+
+	printf("ch = %c\n", ch);
+
+	switch (ch) {
+
+	case '+': 
+
+	f = a + b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '-': 
+
+	f = a - b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '*': 
+
+	f = a * b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '/': 
+
+	f = a / b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	default: 
+
+	printf("invalid operator\n");
+
+		break;
+
+	}
+
+	ch = '-';
+
+	printf("ch = %c\n", ch);
+
+	switch (ch) {
+
+	case '+': 
+
+	f = a + b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '-': 
+
+	f = a - b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '*': 
+
+	f = a * b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '/': 
+
+	f = a / b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	default: 
+
+	printf("invalid operator\n");
+
+		break;
+
+	}
+
+	ch = '*';
+
+	printf("ch = %c\n", ch);
+
+	switch (ch) {
+
+	case '+': 
+
+	f = a + b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '-': 
+
+	f = a - b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '*': 
+
+	f = a * b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '/': 
+
+	f = a / b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	default: 
+
+	printf("invalid operator\n");
+
+		break;
+
+	} 
+
+	ch ='/';
+
+	printf("ch = %c\n", ch);
+
+	switch (ch) {
+
+	case '+': 
+
+	f = a + b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '-': 
+
+	f = a - b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '*': 
+
+	f = a * b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '/': 
+
+	f = a / b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	default: 
+
+	printf("invalid operator\n");
+
+		break;
+
+	} 
+
+	ch ='%';
+
+	printf("ch = %c\n", ch);
+
+	switch (ch) {
+
+	case '+': 
+
+	f = a + b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '-': 
+
+	f = a - b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '*': 
+
+	f = a * b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	case '/': 
+
+	f = a / b; 
+
+	printf("f = %f\n", f);
+
+		break;
+
+	default: 
+
+	printf("invalid operator\n");
+
+		break;
+
+ 	}
+
+
+}
